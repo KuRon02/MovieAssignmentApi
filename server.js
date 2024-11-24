5a3dcc67c11ba8b55cf6eb9b04b68681b0c7167a
KuRon02 <n3kuro00@students.oamk.fi>
2024-11-24 23:18:40 +0200

    Lisäsin User endpoints

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+var app = express();
+
+const port = process.env.port || 3001;
+
+// Raw Endpoints
+
+// GET / A simple welcome endpoint
+
+app.get('/', (req, res)=> res.send('Welcome to the Movie API!'));
+
+// GET /movie - Get all movies
+app.get('/movie', (req, res) => {
+    res.status(200).json([
+        { movieId, movieName: 'The Terminator', movieYear: 1984, genreName: 'Action' },
+        { movieId, movieName: 'Borat', movieYear: 2006, genreName: 'Comedy' },
+        { movieId, movieName: 'Interstellar', movieYear: 2014, genreName: 'Drama' }
+    ]);
+});
+
+// Start the server
+app.listen(3001, () => {
+    console.log('Server running on http://localhost:3001');
+});
